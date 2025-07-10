acecb128b31c3a0ecacfd261a1830e7dd0af5643
s1f10240258 <s1f102402585@iniad.org>
2025-07-10 14:53:33 +0900

    かなり大きめのエラーを即時修正しました。

--- todo/views.py
+++ todo/views.py
@@ -31,12 +31,12 @@ def detail(request, task_id):
     return render(request, "todo/detail.html", context)
 
 def delete(request, task_id):
-  try:
+    try:
         task = Task.objects.get(pk=task_id)
-  except Task.DoesNotExist:
+    except Task.DoesNotExist:
         raise Http404("Task does not exist")
     task.delete()
-    return redirect(index)    
+    return redirect(index)
   
 def close(request, task_id):
     try:
